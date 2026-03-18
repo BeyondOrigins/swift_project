4c9f1a575f923ffb813bd39a97ee58a0ce727e54
Dmitrij Ivlev <dmanivlev@edu.hse.ru>
2026-03-18 21:01:03 +0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by beyondorigins on 22.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
